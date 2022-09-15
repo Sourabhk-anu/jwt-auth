a0cd8e2453d1929d14602cc7cebe90ede354d012
sourabh kumar <sk1272883@gmail.com>
2022-09-15 13:34:00 +0530

    code modified

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const connection = require('./db')
 const userRoutes = require('./routes/users')
 const authRoutes = require('./routes/auth')
+const path = require('path')
 
 connection();
 
@@ -16,9 +17,7 @@ app.use(cors());
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 
-if (process.env.NODE_ENV == 'production'){
-    app.use(express.static('client/build'))
-}
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(port, () => 
     console.log(`listening on port ${port}`)
